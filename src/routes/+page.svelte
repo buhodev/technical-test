d9c4a727a00c0dfc3bb0af36807339957dc78a4d
Carol Hernandez <carolain.hernandez27605@gmail.com>
2024-07-16 17:11:49 -0400

    On branch feature/carol-assignment-branch   push test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <main class="flex h-full items-center justify-center">
-	<h1 class="w-fit rounded-lg bg-secondary-400 p-4 text-white">Your work goes on this page</h1>
+	<div class="flex gap-4">carol test</div>
 </main>
