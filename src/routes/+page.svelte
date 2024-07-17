1636091a158912a05a2b195d8d1e69fd68863e63
Carol Hernandez <carolain.hernandez27605@gmail.com>
2024-07-17 13:49:14 -0400

    Changes to be committed:     modified:   src/routes/+page.svelte       layout the basic structure of the section       write the copy for the section 	new file:   static/cost-management-image.png

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,19 @@
-<main class="flex h-full items-center justify-center">
-	<div class="flex gap-4">carol test</div>
+<main class="flex h-flil items-center justify-center">
+	<section class="flex items-center gap-8 m-auto self-center h-screen">
+		<div class="flex flex-col">
+		<h2 class="">Optimize Your Cloud Spend with Granular Cost Management</h2>
+		<p class="pretty">Gain complete visibility and control over your Kubernetes costs with our comprehensive cost management features. Our platform provides you with the tools you need to:</p>
+		<ul>
+			<li>Track and analyze your Kubernetes spending</li>
+			<li>Identify and eliminate cost inefficiencies</li>
+			<li>Set and enforce cost budgets</li>
+			<li>Allocate costs to specific teams or projects</li>
+			<li>Forecast future cloud expenses</li>
+		</ul>
+		<div class="flex">
+			<button>Book a free demo</button>
+		</div>
+	</div>
+		<div></div>
+	</section>
 </main>
