2e96e24cbdf823f9fc03921cc37263eab35b6c0f
Carol Hernandez <carolain.hernandez27605@gmail.com>
2024-07-17 14:57:59 -0400

    Changes to be committed: 	modified:   src/routes/+page.svelte        added background gradient        polished the layout        polished some styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,26 @@
-<main class="flex h-flil items-center justify-center">
-	<section class="flex items-center gap-8 m-auto px-20 self-center h-screen bg-surface-900">
-		<div class="flex flex-col basis-1/2 gap-6">
-		<h2 class="text-4xl font-bold text-white">Optimize Your Cloud Spend with Granular Cost Management</h2>
-		<p class="pretty text-xl text-gray-300">Gain complete visibility and control over your Kubernetes costs with our comprehensive cost management features. Our platform provides you with the tools you need to:</p>
-		<ul class="text-gray-300">
-			<li>Track and analyze your Kubernetes spending</li>
-			<li>Identify and eliminate cost inefficiencies</li>
-			<li>Set and enforce cost budgets</li>
-			<li>Allocate costs to specific teams or projects</li>
-			<li>Forecast future cloud expenses</li>
-		</ul>
-		<div class="flex">
-			<a href="#" class="btn">Book a free demo</a>
+<main class="flex h-full items-center justify-center bg-surface-900">
+	<section >
+	
+		<div class="flex items-center px-12 gap-8 max-w-7xl m-auto z-10">
+			<div class="flex flex-col basis-3/5 gap-6">
+				<h2 class="text-4xl font-bold text-white">Optimize Your Cloud Spend with Granular Cost Management</h2>
+				<p class="pretty text-xl text-gray-300">Gain complete visibility and control over your Kubernetes costs with our comprehensive cost management features. Our platform provides you with the tools you need to:</p>
+				<ul class="text-gray-300">
+					<li>Track and analyze your Kubernetes spending</li>
+					<li>Identify and eliminate cost inefficiencies</li>
+					<li>Set and enforce cost budgets</li>
+					<li>Allocate costs to specific teams or projects</li>
+					<li>Forecast future cloud expenses</li>
+				</ul>
+				<div class="flex">
+					<a href="#" class="btn">Book a free demo</a>
+				</div>
+			</div>
+				<div>
+					<img src="/cost-management-image.png" alt=" Cost Management Feature">
+				</div>
 		</div>
-	</div>
-		<div>
-			<img src="/cost-management-image.png" alt=" Cost Management Feature">
-		</div>
+		
+		<div class="w-full h-full inset-0 absolute blur-3xl" style="background:linear-gradient(202.72deg, rgba(237, 78, 139, 0.1) 14.76%, rgba(152, 103, 240, 0.09) 34.37%, rgba(152, 103, 240, 0) 86.62%, rgba(0, 0, 255, 0.1) 100%)"></div>
 	</section>
 </main>
