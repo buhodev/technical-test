ab68cb3b93a199a32bc4e772424649165608c7df
Carol Hernandez <carolain.hernandez27605@gmail.com>
2024-07-17 16:22:11 -0400

    Changes to be committed: 	modified:   src/routes/+page.svelte         from HTML to svelte component 	new file:   src/routes/feature.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,22 @@
+<script>
+	import Feature from './feature.svelte';
+</script>
+
 <main class="flex h-full items-center justify-center bg-surface-900">
-	<section >
-		<div class="w-full h-full inset-0 absolute blur-3xl pointer-events-none hidden" style="background:linear-gradient(202.72deg, rgba(237, 78, 139, 0.1) 14.76%, rgba(152, 103, 240, 0.09) 34.37%, rgba(152, 103, 240, 0) 86.62%, rgba(0, 0, 255, 0.1) 100%)"></div>
-		<div class="flex max-md:flex-col items-center px-6 lg:px-12 py-20 gap-8 max-w-7xl m-auto z-10">
-			<div class="flex flex-col basis-3/5 gap-3 md:gap-6">
-				<h2 class="text-2xl xl:text-4xl font-bold text-white">Optimize Your Cloud Spend with Granular Cost Management</h2>
-				<p class="text-pretty text-base xl:text-xl text-gray-300">Gain complete visibility and control over your Kubernetes costs with our comprehensive cost management features. Our platform provides you with the tools you need to:</p>
-				<ul class="text-gray-300 gap-2 md:gap-3 flex flex-col">
-					<li>Track and analyze your Kubernetes spending</li>
-					<li>Identify and eliminate cost inefficiencies</li>
-					<li>Set and enforce cost budgets</li>
-					<li>Allocate costs to specific teams or projects</li>
-					<li>Forecast future cloud expenses</li>
-				</ul>
-				<div class="flex pt-4">
-					<a href="#" class="btn">Book a free demo</a>
-				</div>
-			</div>
-				<div class="max-w-xl flex justify-end">
-					<img src="/cost-management-image.png" alt=" Cost Management Feature" class="max-w-3xl max-xl:w-full  ">
-				</div>
-		</div>
-		
-	</section>
+	<Feature
+		title="Optimize Your Cloud Spend with Granular Cost Management"
+		description="Gain complete visibility and control over your Kubernetes costs with our comprehensive cost management features. Our platform provides you with the tools you need to:"
+		ctaText="Book a free demo"
+		ctaLink="#"
+		imageSrc="/cost-management-image.png"
+		imageAlt=" Cost Management Feature"
+	><slot>
+		<ul class="flex flex-col gap-2 text-gray-300 md:gap-3">
+			<li>Track and analyze your Kubernetes spending</li>
+			<li>Identify and eliminate cost inefficiencies</li>
+			<li>Set and enforce cost budgets</li>
+			<li>Allocate costs to specific teams or projects</li>
+			<li>Forecast future cloud expenses</li>
+		</ul></slot></Feature
+	>
 </main>
