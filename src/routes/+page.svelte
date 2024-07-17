613af1e1d0dbb209779b00464b208eafac714c29
Carol Hernandez <carolain.hernandez27605@gmail.com>
2024-07-17 17:29:26 -0400

    Changes to be committed: 	modified:   src/routes/+page.svelte      added the icons to the especifications of the feature 	new file:   src/routes/checkIcon.svelte         Finished the design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Feature from './feature.svelte';
+	import CheckIcon from './checkIcon.svelte';
 </script>
 
 <main class="flex h-full items-center justify-center bg-surface-900">
@@ -9,14 +10,14 @@
 		ctaText="Book a free demo"
 		ctaLink="#"
 		imageSrc="/cost-management-image.png"
-		imageAlt=" Cost Management Feature"
+		imageAlt="Cost Management Feature"
 	><slot>
 		<ul class="flex flex-col gap-2 text-gray-300 md:gap-3">
-			<li>Track and analyze your Kubernetes spending</li>
-			<li>Identify and eliminate cost inefficiencies</li>
-			<li>Set and enforce cost budgets</li>
-			<li>Allocate costs to specific teams or projects</li>
-			<li>Forecast future cloud expenses</li>
+			<li class="pl-8 relative "> <CheckIcon class="absolute left-0 top-0.5 size-5 text-secondary-400"/> Track and analyze your Kubernetes spending</li>
+			<li class="pl-8 relative"> <CheckIcon class="absolute left-0 top-0.5 size-5 text-secondary-400"/>  Identify and eliminate cost inefficiencies</li>
+			<li class="pl-8 relative"><CheckIcon class="absolute left-0 top-0.5 size-5 text-secondary-400"/>  Set and enforce cost budgets</li>
+			<li class="pl-8 relative"><CheckIcon class="absolute left-0 top-0.5 size-5 text-secondary-400"/>  Allocate costs to specific teams or projects</li>
+			<li class="pl-8 relative"><CheckIcon class="absolute left-0 top-0.5 size-5 text-secondary-400"/>  Forecast future cloud expenses</li>
 		</ul></slot></Feature
 	>
 </main>
--- src/routes/feature.svelte
+++ src/routes/feature.svelte
@@ -7,7 +7,7 @@
     export let imageAlt;
   </script>
 <section >
-    <div class="w-full h-full inset-0 absolute blur-3xl pointer-events-none hidden" style="background:linear-gradient(202.72deg, rgba(237, 78, 139, 0.1) 14.76%, rgba(152, 103, 240, 0.09) 34.37%, rgba(152, 103, 240, 0) 86.62%, rgba(0, 0, 255, 0.1) 100%)"></div>
+    <div class="w-full h-full inset-0 absolute blur-3xl pointer-events-none" style="background:linear-gradient(202.72deg, rgba(237, 78, 139, 0.1) 14.76%, rgba(152, 103, 240, 0.09) 34.37%, rgba(152, 103, 240, 0) 86.62%, rgba(0, 0, 255, 0.1) 100%)"></div>
     <div class="flex max-md:flex-col items-center px-6 lg:px-12 py-20 gap-8 max-w-7xl m-auto z-10">
         <div class="flex flex-col basis-3/5 gap-3 md:gap-6">
             <h2 class="text-2xl xl:text-4xl font-bold text-white">{title}</h2>
