313601387b162acc2105a8e35967f6e436d07ee3
Carol Hernandez <carolain.hernandez27605@gmail.com>
2024-07-17 14:32:49 -0400

    Changes to be committed: modified:   src/app.css     adding variables for colors     adding styles for buttons modified:   src/routes/+page.svelte     adding color styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <main class="flex h-flil items-center justify-center">
-	<section class="flex items-center gap-8 m-auto self-center h-screen">
-		<div class="flex flex-col">
-		<h2 class="">Optimize Your Cloud Spend with Granular Cost Management</h2>
-		<p class="pretty">Gain complete visibility and control over your Kubernetes costs with our comprehensive cost management features. Our platform provides you with the tools you need to:</p>
-		<ul>
+	<section class="flex items-center gap-8 m-auto px-20 self-center h-screen bg-surface-900">
+		<div class="flex flex-col basis-1/2 gap-6">
+		<h2 class="text-4xl font-bold text-white">Optimize Your Cloud Spend with Granular Cost Management</h2>
+		<p class="pretty text-xl text-gray-300">Gain complete visibility and control over your Kubernetes costs with our comprehensive cost management features. Our platform provides you with the tools you need to:</p>
+		<ul class="text-gray-300">
 			<li>Track and analyze your Kubernetes spending</li>
 			<li>Identify and eliminate cost inefficiencies</li>
 			<li>Set and enforce cost budgets</li>
@@ -11,9 +11,11 @@
 			<li>Forecast future cloud expenses</li>
 		</ul>
 		<div class="flex">
-			<button>Book a free demo</button>
+			<a href="#" class="btn">Book a free demo</a>
 		</div>
 	</div>
-		<div></div>
+		<div>
+			<img src="/cost-management-image.png" alt=" Cost Management Feature">
+		</div>
 	</section>
 </main>
